9fedfc7711693538813c748d2e694b8862a42986
BalajiArighant <60035773+BalajiArighant@users.noreply.github.com>
2025-02-15 20:25:27 +0530

    Change vercel.json

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "chatbot.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv('DB_NAME'),
 		"HOST": os.getenv('DB_HOST'),
 		"USER": os.getenv('DB_USER'),
